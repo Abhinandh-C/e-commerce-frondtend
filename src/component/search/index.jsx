88574e365b040219e1e6aa2bce72e24236dc4837
Abhinandh <abhinandh20011@gmail.com>
2025-02-24 17:50:48 +0530

    search box for headerBox

---
+++ src/component/search/index.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+import { IoSearchOutline } from "react-icons/io5";
+import { TypeAnimation } from 'react-type-animation';
+import { useState,useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+
+const SearchBar = () => {
+    const navigator = useNavigate()
+    const location = useLocation()  
+    const [searchPage, setSearchPage] = useState(false)
+
+    useEffect(()=>{
+        const isSearch = location.pathname === '/search'
+        setSearchPage(isSearch)
+      },[location])
+      const handleRedirectNavigator =()=>{
+        navigator('search')
+      }    
+
+
+
+  return (
+      <div className='w-full h-full'>
+      {
+        !searchPage ? (
+          // not in search page
+      <div onClick= {handleRedirectNavigator} className='animation '>
+      <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed out once, initially
+        'Search for Mice',
+        1000, // wait 1s before replacing "Mice" with "Hamsters"
+        'Search for Hamsters',
+        1000,
+        'Search for Guinea Pigs',
+        1000,
+        'Search for Chinchillas',
+        1000
+      ]}
+      wrapper="span"
+      speed={50}
+      style={{ fontSize: '2em', display: 'inline-block' }}
+      repeat={Infinity}
+    />
+    </div>
+
+):(
+    // in search page
+    <div className='searchbar '  >
+      <input type="text" placeholder='Search for products' />
+    </div>
+  )
+}
+
+    
+      
+      
+      <Button><  IoSearchOutline /></Button>
+    </div>
+  )
+}
+
+export default SearchBar
