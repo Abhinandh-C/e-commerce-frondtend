5eabb16cfcf564099b88525c4a4a77286821f9d3
Abhinandh <abhinandh20011@gmail.com>
2025-03-03 21:40:24 +0530

    Navigation bar has been completed all

--- src/component/Header/Navigation/index.jsx
+++ src/component/Header/Navigation/index.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from '@mui/material';
 import { IoMdMenu } from 'react-icons/io';
 import { FaAngleDown } from 'react-icons/fa6';
 import { Link } from 'react-router-dom';
+import { GiTv } from "react-icons/gi";
+import { CiMobile3 } from "react-icons/ci";
+import { GiAmpleDress } from "react-icons/gi";
+import { GiWashingMachine } from "react-icons/gi";
 
 const Navigation = () => {
+
+const[isOpenSideBar,setisOpenSideBar]=useState(false);
+
+
+
   return (
     <nav>
       <div className="container">
-        <div className="row align-items-center">
+        <div className="row align-items-center" >
           {/* Left section */}
           <div className="col-6 col-md-3 navpart1">
-            <Button className="allcartTab d-flex align-items-center">
+            <div className='catWrapper'>
+            <Button className="allcartTab d-flex align-items-center" onClick={()=>setisOpenSideBar(!isOpenSideBar)}>
               <span className="icon me-2">
                 <IoMdMenu size={24} />
               </span>
@@ -20,18 +30,80 @@ const Navigation = () => {
                 <FaAngleDown size={18} />
               </span>
             </Button>
+
+            { <div className={`sideBarNav ${isOpenSideBar===true ?'open' : ''}`}>
+              <ul>
+                <li><Link to="/"><Button>Fashion</Button></Link></li>
+                <li><Link to="/"><Button>Electronics</Button></Link></li>
+                <li><Link to="/"><Button>Mobile</Button></Link></li>
+                <li><Link to="/"><Button>Appliances</Button></Link></li>
+                <li><Link to="/"><Button>furniture</Button></Link></li>
+                <li><Link to="/"><Button>Two Wheelers</Button></Link></li>
+                <li><Link to="/"><Button>Fashion</Button></Link></li>
+                <li><Link to="/"><Button>Electronics</Button></Link></li>
+                <li><Link to="/"><Button>Mobile</Button></Link></li>
+                <li><Link to="/"><Button>Appliances</Button></Link></li>
+                <li><Link to="/"><Button>furniture</Button></Link></li>
+                <li><Link to="/"><Button>Two Wheelers</Button></Link></li>
+              </ul>
+            </div> }
+
+            </div>
           </div>
 
           {/* Right section */}
           <div className="col-6 col-md-9 navpart2 d-flex align-items-center">
             <ul className="list list-inline d-flex flex-wrap justify-content-end m-0 p-0">
-              <li className="list-inline-item px-2"> <Link to="/">Home</Link></li>
-              <li className="list-inline-item px-2"> <Link to="/">Fashion</Link></li>
-              <li className="list-inline-item px-2"> <Link to="/">Electronics</Link></li>
-              <li className="list-inline-item px-2"> <Link to="/">MOBILES</Link></li>
-              <li className="list-inline-item px-2"> <Link to="/">Appliances</Link></li>
-              <li className="list-inline-item px-2"> <Link to="/">Blog</Link></li>
-              <li className="list-inline-item px-2"> <Link to="/">Contact Us</Link></li>
+              <li className="list-inline-item px-2"> <Link to="/"><Button>Home</Button></Link></li>
+
+
+              <li className="list-inline-item px-2"> <Link to="/"><Button><GiAmpleDress/>&nbsp;Fashion</Button></Link>
+              
+              <div className='submenu shadow'>
+                <Link to="/"><Button>Clothing</Button></Link>
+                <Link to="/"><Button>Footwear</Button></Link>
+                <Link to="/"><Button>Watches</Button></Link>
+                <Link to="/"><Button>Bags & Suitcase</Button></Link>
+                <Link to="/"><Button>Winter</Button></Link>
+              </div>
+              </li>
+
+              <li className="list-inline-item px-2"> <Link to="/"><Button><GiTv/>&nbsp;Electronics</Button></Link>
+              
+              <div className='submenu shadow'>
+                <Link to="/"><Button>Audio</Button></Link>
+                <Link to="/"><Button>Gaming</Button></Link>
+                <Link to="/"><Button>Laptops & Desktops</Button></Link>
+                <Link to="/"><Button>Camera & Accessories</Button></Link>
+                <Link to="/"><Button>Smart Hearings</Button></Link>
+              </div>
+              </li>
+
+              <li className="list-inline-item px-2"> <Link to="/"><Button><CiMobile3/>&nbsp;MOBILES</Button></Link>
+              
+              <div className='submenu shadow'>
+                <Link to="/"><Button>I Phone</Button></Link>
+                <Link to="/"><Button>Samsung</Button></Link>
+                <Link to="/"><Button>Huwai</Button></Link>
+                <Link to="/"><Button>Vivo</Button></Link>
+                <Link to="/"><Button>Oppo</Button></Link>
+              </div> 
+              </li>
+
+              <li className="list-inline-item px-2"> <Link to="/"><Button><GiWashingMachine/>&nbsp;Appliances</Button></Link>
+              
+              <div className='submenu shadow'>
+                <Link to="/"><Button>Washing Mechines</Button></Link>
+                <Link to="/"><Button>Micro Owen</Button></Link>
+                <Link to="/"><Button>Refrigerators </Button></Link>
+                <Link to="/"><Button>Smart Televisions</Button></Link>
+                <Link to="/"><Button>Air Conditioners</Button></Link>
+              </div>
+              </li>
+
+              
+              <li className="list-inline-item px-2"> <Link to="/"><Button>Blog</Button></Link></li>
+              <li className="list-inline-item px-2"> <Link to="/"><Button>Contact Us</Button></Link></li>
             </ul>
           </div>
         </div>
